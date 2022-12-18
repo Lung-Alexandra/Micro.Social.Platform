e1e42e36d1e44d714edf5a19628f167be497fb3b
visan <alexandruvisan44@yahoo.com>
2022-12-18 11:44:15 +0200

    Implemented profile and home into menubar.

--- MicroSocialPlatform/Controllers/StartController.cs
+++ MicroSocialPlatform/Controllers/StartController.cs
@@ -2,14 +2,26 @@
 using MicroSocialPlatform.Data;
 using MicroSocialPlatform.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 
 namespace MicroSocialPlatform.Controllers;
 
 public class StartController : Controller
 {
+    private readonly SignInManager<AppUser> _signInManager;
+    public StartController(SignInManager<AppUser> signInManager)
+    {
+        _signInManager= signInManager;
+    }
+
     // The start page.
     public IActionResult Index()
     {
+        // If the user is signed in, redirect to the home page.
+        if (_signInManager.IsSignedIn(User))
+        {
+            return RedirectToRoute("home");
+        }
         return View();
     }
 
