d46bee06c6ba43f66acd8aa23f0e39d4896eaa8b
visan <alexandruvisan44@yahoo.com>
2023-01-08 11:23:27 +0200

    Precompute required data in the home controller before sending it to post partial.

--- MicroSocialPlatform/Controllers/HomeController.cs
+++ MicroSocialPlatform/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using MicroSocialPlatform.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace MicroSocialPlatform.Controllers;
@@ -9,10 +10,12 @@ using Microsoft.AspNetCore.Mvc;
 public class HomeController : Controller
 {
     private readonly ApplicationDbContext _db;
+    private readonly UserManager<AppUser> _userManager;
 
-    public HomeController(ApplicationDbContext db)
+    public HomeController(ApplicationDbContext db, UserManager<AppUser> userManager)
     {
         _db = db;
+        _userManager = userManager;
     }
 
     // Show all posts.
@@ -26,6 +29,18 @@ public class HomeController : Controller
             .ThenInclude(c => c.User)
             .ThenInclude(u => u.UserProfile)
             .ToList();
+
+        bool admin = User.IsInRole("Admin");
+        string myId = _userManager.GetUserId(User);
+
+        foreach (var post in posts)
+        {
+            // Set the number of comments.
+            post.numComments = post.Comments.Count;
+            // The post can be edited if the current user is an admin or owns the post.
+            post.userCanEdit = myId == post.UserId || admin;
+        }
+
         var model = new HomeView(posts);
         return View(model);
     }
